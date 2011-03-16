5a6ca84a15910dcc33689a7d37a8d7b785a06300
mavrik <none@none>
2011-03-16 19:35:00 +0100

    Finished search form GUI
    --HG--
    branch : Redesign

--- src/org/prevoz/android/search/MainActivity.java
+++ src/org/prevoz/android/search/MainActivity.java
@@ -1,24 +1,27 @@
 package org.prevoz.android.search;
 
+import java.util.Calendar;
+
 import org.prevoz.android.R;
 
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
+import android.support.v4.app.FragmentManager;
+import android.widget.DatePicker;
 
-public class MainActivity extends FragmentActivity
+public class MainActivity extends FragmentActivity implements OnDateSetListener
 {
-	// Activity form fields
-	private Button buttonDate;
-	private AutoCompleteTextView fromField;
-	private AutoCompleteTextView toField;
+	public static final int DIALOG_SEARCH_DATE = 0;
 	
-	private void prepareFormFields()
+	// For picking search date
+	private DatePickerDialog datePicker;
+	
+	public void startSearch(String from, String to, Calendar when)
 	{
-		buttonDate = (Button)findViewById(R.id.date_button);
-		fromField = (AutoCompleteTextView)findViewById(R.id.from_field);
-		toField = (AutoCompleteTextView)findViewById(R.id.to_field);
+		
 	}
 	
 	/** Called when the activity is first created. */
@@ -27,7 +30,31 @@ public class MainActivity extends FragmentActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
+		
+		Calendar now = Calendar.getInstance();
+		datePicker = new DatePickerDialog(this, this, now.get(Calendar.YEAR), now.get(Calendar.MONTH), now.get(Calendar.DAY_OF_MONTH));
 	}
 
+	public void onDateSet(DatePicker picker, int year, int month, int dayOfMonth) 
+	{
+		Calendar date = Calendar.getInstance();
+		date.set(Calendar.YEAR, year);
+		date.set(Calendar.MONTH, month);
+		date.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+		
+		SearchFormFragment searchForm = (SearchFormFragment)getSupportFragmentManager().findFragmentById(R.id.search_form_fragment);
+		searchForm.setSelectedDate(date);
+	}
 
+	@Override
+	protected Dialog onCreateDialog(int id) 
+	{
+		switch(id)
+		{
+			case DIALOG_SEARCH_DATE:
+				return datePicker;
+			default:
+				return super.onCreateDialog(id);
+		}
+	}
 }
--- src/org/prevoz/android/search/SearchFormFragment.java
+++ src/org/prevoz/android/search/SearchFormFragment.java
@@ -1,11 +1,16 @@
 package org.prevoz.android.search;
 
-import org.prevoz.android.R;
+import java.util.Calendar;
 
+import org.prevoz.android.R;
+import org.prevoz.android.util.LocaleUtil;
+
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -16,22 +21,13 @@ public class SearchFormFragment extends Fragment
 	private AutoCompleteTextView fromField;
 	private AutoCompleteTextView toField;
 	
+	private Calendar selectedDate;
 	
 	private void prepareFormFields()
 	{
 		buttonDate = (Button)getActivity().findViewById(R.id.date_button);
 		fromField = (AutoCompleteTextView)getActivity().findViewById(R.id.from_field);
 		toField = (AutoCompleteTextView)getActivity().findViewById(R.id.to_field);
-	}
-	
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) 
-	{
-		super.onActivityCreated(savedInstanceState);
-		prepareFormFields();
-		
-		// Initialize date display
-		buttonDate.setText("danes");
 		
 		// Initialize locations autocomplete
 		LocationAutocompleteAdapter locAdapter = new LocationAutocompleteAdapter(getActivity(), null);
@@ -41,6 +37,27 @@ public class SearchFormFragment extends Fragment
 		// Set treshold for autocomplete
 		fromField.setThreshold(1);
 		toField.setThreshold(1);
+		
+		// Set initial date
+		buttonDate.setText(localizeDate(selectedDate));
+		buttonDate.setOnClickListener(new OnClickListener() 
+		{
+			
+			public void onClick(View v) 
+			{
+				getActivity().showDialog(MainActivity.DIALOG_SEARCH_DATE);
+			}
+		});
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) 
+	{
+		super.onActivityCreated(savedInstanceState);
+		
+		selectedDate = Calendar.getInstance();
+		
+		prepareFormFields();
 	}
 
 	@Override
@@ -52,5 +69,45 @@ public class SearchFormFragment extends Fragment
 		return newView;
 	}
 	
+	/**
+	 * Builds a localized date string with day name
+	 */
+	private String localizeDate(Calendar date)
+	{
+		Resources resources = getResources();
+		
+		Calendar now = Calendar.getInstance();
+		// Check for today and tomorrow
+		if (date.get(Calendar.ERA) == now.get(Calendar.ERA) && 
+			date.get(Calendar.YEAR) == now.get(Calendar.YEAR))
+		{
+			// Today
+			if (date.get(Calendar.DAY_OF_YEAR) == now.get(Calendar.DAY_OF_YEAR))
+			{
+				return resources.getString(R.string.today);
+			}
+			
+			// Add one day to now to get tomorrows date
+			now.roll(Calendar.DAY_OF_YEAR, 1);
+			
+			// Tomorrow, because we added one day to now
+			if (date.get(Calendar.DAY_OF_YEAR) == now.get(Calendar.DAY_OF_YEAR))
+			{
+				return resources.getString(R.string.tomorrow);
+			}
+		}
+
+		StringBuilder dateString = new StringBuilder();
+
+		dateString.append(LocaleUtil.getDayName(resources, date) + ", ");
+		dateString.append(LocaleUtil.getFormattedDate(resources, date));
+
+		return dateString.toString();
+	}
 	
+	public void setSelectedDate(Calendar date)
+	{
+		this.selectedDate = date;
+		buttonDate.setText(localizeDate(selectedDate));
+	}
 }
