3446b028ca73d4a2d691814f6e1a63f4f6b309e8
mavrik <jernej@virag.si>
2011-06-26 20:43:51 +0200

    Refactored RideInfo Activity to expose data fill utility class
    --HG--
    branch : Last searched rides

--- src/org/prevoz/android/rideinfo/RideInfoActivity.java
+++ src/org/prevoz/android/rideinfo/RideInfoActivity.java
@@ -1,12 +1,8 @@
 package org.prevoz.android.rideinfo;
 
-import java.text.SimpleDateFormat;
-
 import org.prevoz.android.R;
-import org.prevoz.android.util.LocaleUtil;
 
 import android.content.Intent;
-import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -16,36 +12,17 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 public class RideInfoActivity extends FragmentActivity implements LoaderCallbacks<Ride>
 {
-	// UI fields
-	private ViewFlipper rideFlipper;
-	
-	private TextView fromText;
-	private TextView toText;
-	private TextView timeText;
-	private TextView dayText;
-	private TextView dateText;
-	private TextView priceText;
-	private TextView pplText;
-	private TextView pplTagText;
-	private TextView driverText;
-	private TextView contactText;
-	private TextView commentText;
-	
-	private Button callButton;
-	private Button smsButton;
-	private Button delButton;
-	
 	public static final String RIDE_ID = RideInfoActivity.class.toString() + ".ride_id";
 	
-	private int rideID;
-	private Ride ride = null;
+	private Ride ride;
+	private RideInfoUtil rideInfoUtil;
+	
+	public ViewFlipper rideFlipper;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -54,106 +31,21 @@ public class RideInfoActivity extends FragmentActivity implements LoaderCallback
 		setContentView(R.layout.ride_info_activity);
 		prepareUIElements();
 		
-		rideFlipper.setInAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_in));
-		rideFlipper.setOutAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_out));
-		rideFlipper.setDisplayedChild(0);
-		
 		// Get ride ID
 		if (getIntent().getExtras() != null)
 		{
-			rideID = getIntent().getExtras().getInt(RIDE_ID);
+			int rideID = getIntent().getExtras().getInt(RIDE_ID);
 			Log.d(this.toString(), "Requesting ride info for id " + rideID);
+			getSupportLoaderManager().initLoader(rideID, null, this);
 		}
-		
-		getSupportLoaderManager().initLoader(rideID, null, this);
 	}
 	
 	private void prepareUIElements()
 	{
-		// UI fields
-		rideFlipper = (ViewFlipper)findViewById(R.id.rideinfo_flipper);
-		
-		fromText = (TextView) findViewById(R.id.rideinfo_from);
-		toText = (TextView) findViewById(R.id.rideinfo_to);
-		timeText = (TextView) findViewById(R.id.rideinfo_time);
-		dayText = (TextView) findViewById(R.id.rideinfo_day);
-		dateText = (TextView) findViewById(R.id.rideinfo_date);
-		priceText = (TextView) findViewById(R.id.rideinfo_price);
-		pplText = (TextView) findViewById(R.id.rideinfo_people);
-		pplTagText = (TextView) findViewById(R.id.rideinfo_peopletag);
-		driverText = (TextView) findViewById(R.id.rideinfo_author);
-		contactText = (TextView) findViewById(R.id.rideinfo_phone);
-		commentText = (TextView) findViewById(R.id.rideinfo_comment);
-		
-		callButton = (Button)findViewById(R.id.rideinfo_call);
-		smsButton = (Button)findViewById(R.id.rideinfo_sms);
-		delButton = (Button) findViewById(R.id.rideinfo_delsend);
-	}
-
-	private void showRide(Ride ride)
-	{
-		Resources res = getResources();
-		
-		// From and to
-		fromText.setText(ride.getFrom());
-		toText.setText(ride.getTo());
-
-		// Time and date
-		SimpleDateFormat timeFormatter = new SimpleDateFormat("HH:mm");		
-		timeText.setText(timeFormatter.format(ride.getTime()));
-		dayText.setText(LocaleUtil.getDayName(res, ride.getTime()) + ",");
-		dateText.setText(LocaleUtil.getFormattedDate(res, ride.getTime()));
-
-		// Price and number of people
-		if (ride.getPrice() != null)
-		{
-			priceText.setText(String.format("%1.1f €", ride.getPrice()));
-		}
-		else
-		{
-			priceText.setText("?");
-		}
-
-		
-		pplText.setText(String.valueOf(ride.getPeople()));
-		pplTagText.setText(LocaleUtil.getStringNumberForm(res,
-				R.array.people_tags, ride.getPeople()));
-
-		// Driver and contact		
-		if (ride.getAuthor() == null || ride.getAuthor().trim().length() == 0)
-		{
-			driverText.setVisibility(View.GONE);
-		}
-		else
-		{
-			driverText.setVisibility(View.VISIBLE);
-			driverText.setText(ride.getAuthor());
-		}
-
-		// Contact info
-		contactText.setText(ride.getContact());
-
-		// Comment
-		commentText.setText(ride.getComment());
-
-		// Setup button callbacks
-		callButton.setOnClickListener(new OnClickListener()
-		{
-			public void onClick(View v)
-			{
-				callAuthor();
-			}
-		});
-
-		smsButton.setOnClickListener(new OnClickListener()
-		{
-			public void onClick(View v)
-			{
-				sendSMS();
-			}
-		});
-		
-		rideFlipper.showNext();
+		rideFlipper = (ViewFlipper) findViewById(R.id.rideinfo_flipper);
+		rideFlipper.setInAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_in));
+		rideFlipper.setOutAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_out));
+		rideFlipper.setDisplayedChild(0);
 	}
 
 	/**
@@ -178,7 +70,7 @@ public class RideInfoActivity extends FragmentActivity implements LoaderCallback
 
 	public Loader<Ride> onCreateLoader(int loaderID, Bundle args) 
 	{
-		return new RideInfoLoader(this, rideID); 
+		return new RideInfoLoader(this, loaderID); 
 	}
 
 	public void onLoadFinished(Loader<Ride> loader, Ride result) 
@@ -190,7 +82,27 @@ public class RideInfoActivity extends FragmentActivity implements LoaderCallback
 		}
 		
 		this.ride = result;
-		showRide(ride);
+		
+		OnClickListener callAuthor = new OnClickListener()
+		{
+			
+			public void onClick(View v)
+			{
+				callAuthor();
+			}
+		};
+		
+		OnClickListener sendSMS = new OnClickListener()
+		{
+			public void onClick(View v)
+			{
+				sendSMS();
+			}
+		};
+		
+		rideInfoUtil = new RideInfoUtil(this, callAuthor, sendSMS);
+		rideInfoUtil.showRide(ride, true);
+		rideFlipper.showNext();
 	}
 
 	public void onLoaderReset(Loader<Ride> loader) 
---
+++ src/org/prevoz/android/rideinfo/RideInfoUtil.java
@@ -0,0 +1,130 @@
+package org.prevoz.android.rideinfo;
+
+import java.text.SimpleDateFormat;
+
+import org.prevoz.android.R;
+import org.prevoz.android.util.LocaleUtil;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class RideInfoUtil
+{
+	private Activity context;
+	
+	
+	private TextView fromText;
+	private TextView toText;
+	private TextView timeText;
+	private TextView dayText;
+	private TextView dateText;
+	private TextView priceText;
+	private TextView pplText;
+	private TextView pplTagText;
+	private TextView driverText;
+	private TextView contactText;
+	private TextView commentText;
+	private Button callButton;
+	private Button smsButton;
+	private Button delButton;
+
+	private OnClickListener callAuthor;
+	private OnClickListener sendSMS;
+	
+	public RideInfoUtil(Activity context)
+	{
+		this.context = context;
+	}
+	
+	public RideInfoUtil(Activity context, OnClickListener callAuthor, OnClickListener sendSMS)
+	{
+		this.context = context;
+		this.callAuthor = callAuthor;
+		this.sendSMS = sendSMS;
+		
+		prepareUIElements();
+	}
+	
+	private void prepareUIElements()
+	{
+		// UI fields
+		fromText = (TextView) context.findViewById(R.id.rideinfo_from);
+		toText = (TextView) context.findViewById(R.id.rideinfo_to);
+		timeText = (TextView) context.findViewById(R.id.rideinfo_time);
+		dayText = (TextView) context.findViewById(R.id.rideinfo_day);
+		dateText = (TextView) context.findViewById(R.id.rideinfo_date);
+		priceText = (TextView) context.findViewById(R.id.rideinfo_price);
+		pplText = (TextView) context.findViewById(R.id.rideinfo_people);
+		pplTagText = (TextView) context.findViewById(R.id.rideinfo_peopletag);
+		driverText = (TextView) context.findViewById(R.id.rideinfo_author);
+		contactText = (TextView) context.findViewById(R.id.rideinfo_phone);
+		commentText = (TextView) context.findViewById(R.id.rideinfo_comment);
+		
+		callButton = (Button)context.findViewById(R.id.rideinfo_call);
+		smsButton = (Button)context.findViewById(R.id.rideinfo_sms);
+		delButton = (Button) context.findViewById(R.id.rideinfo_delsend);
+	}
+	
+	public void showRide(Ride ride, boolean showControls)
+	{
+		Resources res = context.getResources();
+		
+		// From and to
+		fromText.setText(ride.getFrom());
+		toText.setText(ride.getTo());
+
+		// Time and date
+		SimpleDateFormat timeFormatter = new SimpleDateFormat("HH:mm");		
+		timeText.setText(timeFormatter.format(ride.getTime()));
+		dayText.setText(LocaleUtil.getDayName(res, ride.getTime()) + ",");
+		dateText.setText(LocaleUtil.getFormattedDate(res, ride.getTime()));
+
+		// Price and number of people
+		if (ride.getPrice() != null)
+		{
+			priceText.setText(String.format("%1.1f €", ride.getPrice()));
+		}
+		else
+		{
+			priceText.setText("?");
+		}
+
+		
+		pplText.setText(String.valueOf(ride.getPeople()));
+		pplTagText.setText(LocaleUtil.getStringNumberForm(res,
+				R.array.people_tags, ride.getPeople()));
+
+		// Driver and contact		
+		if (ride.getAuthor() == null || ride.getAuthor().trim().length() == 0)
+		{
+			driverText.setVisibility(View.GONE);
+		}
+		else
+		{
+			driverText.setVisibility(View.VISIBLE);
+			driverText.setText(ride.getAuthor());
+		}
+
+		// Contact info
+		contactText.setText(ride.getContact());
+
+		// Comment
+		commentText.setText(ride.getComment());
+
+		// Setup button callbacks
+		if (showControls)
+		{
+			callButton.setOnClickListener(callAuthor);
+			smsButton.setOnClickListener(sendSMS);
+		}
+		else
+		{
+			callButton.setVisibility(View.INVISIBLE);
+			smsButton.setVisibility(View.VISIBLE);
+		}
+	}
+}
