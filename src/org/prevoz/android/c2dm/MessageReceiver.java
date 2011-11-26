f70216453d60a516587c573b8992a84066dbdd10
Jernej Virag <jernej@virag.si>
2011-11-26 18:52:09 +0100

    Implemented C2DM registration
    --HG--
    branch : Push notifications

---
+++ src/org/prevoz/android/c2dm/MessageReceiver.java
@@ -0,0 +1,48 @@
+package org.prevoz.android.c2dm;
+
+import java.io.IOException;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.c2dm.C2DMBaseReceiver;
+
+public class MessageReceiver extends C2DMBaseReceiver  
+{
+	public MessageReceiver()
+	{
+		super("gandalfar@gmail.com");
+	}
+
+	
+	
+	@Override
+	public void onRegistered(Context context, String registrationId)
+			throws IOException {
+		// TODO Auto-generated method stub
+		super.onRegistered(context, registrationId);
+	}
+
+
+
+	@Override
+	public void onUnregistered(Context context) {
+		// TODO Auto-generated method stub
+		super.onUnregistered(context);
+	}
+
+
+
+	@Override
+	public void onError(Context context, String error) 
+	{
+		Log.e(this.toString(), "C2DM error: " + error);
+	}
+
+	@Override
+	protected void onMessage(Context context, Intent intent) 
+	{
+		Log.i(this.toString(), "C2DM Message received.");
+	}
+}
--- src/org/prevoz/android/c2dm/NotificationManager.java
+++ src/org/prevoz/android/c2dm/NotificationManager.java
@@ -3,7 +3,10 @@ package org.prevoz.android.c2dm;
 import java.util.Calendar;
 import java.util.List;
 
+import org.prevoz.android.R;
 import org.prevoz.android.util.Database;
+
+import com.google.android.c2dm.C2DMessaging;
 
 import android.content.Context;
 import android.util.Log;
@@ -12,6 +15,13 @@ import android.util.Log;
 public class NotificationManager 
 {
 	private static NotificationManager instance;
+	private static Context applicationContext;
+	
+	public static synchronized NotificationManager getInstance(Context context)
+	{
+		applicationContext = context;
+		return getInstance();
+	}
 	
 	public static synchronized NotificationManager getInstance()
 	{
@@ -23,14 +33,32 @@ public class NotificationManager
 		return instance;
 	}
 	
+	private String registrationId = null;
+	
 	private NotificationManager()
 	{
+		if (applicationContext != null)
+		{
+			registrationId = C2DMessaging.getRegistrationId(applicationContext);
+			Log.i(this.toString(), "Device C2DM registration string is " + registrationId);
+			
+			if (registrationId == null || registrationId.trim().length() == 0)
+				C2DMessaging.register(applicationContext, applicationContext.getResources().getString(R.string.c2dm_sender));
+		}
 	}
 
 	public boolean notificationsAvailable()
 	{
-		// TODO implement
-		return true;
+		// Attempt to get registration ID again
+		if (registrationId == null || registrationId.trim().length() == 0)
+		{
+			if (applicationContext != null)
+			{
+				registrationId = C2DMessaging.getRegistrationId(applicationContext);
+			}
+		}
+		
+		return registrationId != null && !registrationId.trim().isEmpty();
 	}
 	
 	public boolean isNotified(Context context, String from, String to, Calendar when) 
