f4a7ebabf8e4eba955316aadad338f9379fcf447
Jernej Virag <jernej@virag.si>
2014-01-25 13:35:22 +0100

    Added search fragment

--- src/main/java/org/prevoz/android/MainActivity.java
+++ src/main/java/org/prevoz/android/MainActivity.java
@@ -1,13 +1,33 @@
 
 package org.prevoz.android;
 
-import com.actionbarsherlock.app.SherlockActivity;
-import com.actionbarsherlock.view.Menu;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.googlecode.androidannotations.annotations.AfterViews;
 import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.FragmentByTag;
+import org.prevoz.android.search.SearchFragment;
+import org.prevoz.android.search.SearchFragment_;
 
 @EActivity(R.layout.activity_main)
-public class MainActivity extends SherlockActivity
+public class MainActivity extends SherlockFragmentActivity
 {
+    private static final String SEARCH_FRAGMENT_TAG = "SearchFragment";
 
+    @FragmentByTag(SEARCH_FRAGMENT_TAG)
+    protected SearchFragment searchFragment;
+
+    @AfterViews
+    protected void initActivity()
+    {
+        // Attach search fragment if it's missing
+        FragmentManager fm = getSupportFragmentManager();
+        if (searchFragment == null)
+        {
+            FragmentTransaction transaction = fm.beginTransaction();
+            transaction.replace(R.id.main_container, new SearchFragment_(), SEARCH_FRAGMENT_TAG);
+            transaction.commit();
+        }
+    }
 }
---
+++ src/main/java/org/prevoz/android/search/SearchFragment.java
@@ -0,0 +1,10 @@
+package org.prevoz.android.search;
+
+import android.support.v4.app.Fragment;
+import com.googlecode.androidannotations.annotations.EFragment;
+import org.prevoz.android.R;
+
+@EFragment(R.layout.fragment_search)
+public class SearchFragment extends Fragment
+{
+}
