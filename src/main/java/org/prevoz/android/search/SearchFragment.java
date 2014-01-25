5fd082384c24d2b5bd19fc9e19b90c5afc860b2e
Jernej Virag <jernej@virag.si>
2014-01-25 21:34:19 +0100

    Added date display

--- src/main/java/org/prevoz/android/search/SearchFragment.java
+++ src/main/java/org/prevoz/android/search/SearchFragment.java
@@ -1,16 +1,33 @@
 package org.prevoz.android.search;
 
 import android.support.v4.app.Fragment;
+import android.widget.EditText;
 import com.fourmob.datetimepicker.date.DatePickerDialog;
-import com.googlecode.androidannotations.annotations.Click;
-import com.googlecode.androidannotations.annotations.EFragment;
+import com.googlecode.androidannotations.annotations.*;
 import org.prevoz.android.R;
+import org.prevoz.android.util.LocaleUtil;
 
 import java.util.Calendar;
 
 @EFragment(R.layout.fragment_search)
 public class SearchFragment extends Fragment implements DatePickerDialog.OnDateSetListener
 {
+    @ViewById(R.id.search_date_edit)
+    protected EditText searchDate;
+
+    @InstanceState
+    protected Calendar selectedDate;
+
+    @AfterViews
+    protected void initFragment()
+    {
+        if (selectedDate == null)
+        {
+            selectedDate = Calendar.getInstance();
+            updateShownDate();
+        }
+    }
+
 
     @Click(R.id.search_date)
     protected void clickDate()
@@ -32,8 +49,17 @@ public class SearchFragment extends Fragment implements DatePickerDialog.OnDateS
     }
 
     @Override
-    public void onDateSet(DatePickerDialog datePickerDialog, int i, int i2, int i3)
+    public void onDateSet(DatePickerDialog datePickerDialog, int year, int month, int day)
     {
+        selectedDate.set(Calendar.YEAR, year);
+        selectedDate.set(Calendar.MONTH, month);
+        selectedDate.set(Calendar.DAY_OF_MONTH, day);
+        updateShownDate();
+    }
 
+
+    private void updateShownDate()
+    {
+        searchDate.setText(LocaleUtil.localizeDate(getResources(), selectedDate));
     }
 }
---
+++ src/main/java/org/prevoz/android/util/LocaleUtil.java
@@ -0,0 +1,135 @@
+package org.prevoz.android.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import org.prevoz.android.R;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+public class LocaleUtil
+{
+    private static HashMap<String, String> localizedCountryNamesCache = new HashMap<String, String>();
+    private static HashMap<String, String> localizedCityNamesCache = new HashMap<String, String>();
+    private static Locale localeCache = null;
+    private static HashMap<String, SimpleDateFormat> dateFormatCache = new HashMap<String, SimpleDateFormat>();
+
+    public static String getDayName(Resources res, Calendar date)
+    {
+        String[] dayNames = res.getStringArray(R.array.day_names);
+        return dayNames[date.get(Calendar.DAY_OF_WEEK) - 1];
+    }
+
+    public static String getShortDayName(Resources res, Calendar date)
+    {
+        String[] shortDayNames = res.getStringArray(R.array.short_day_names);
+        return shortDayNames[date.get(Calendar.DAY_OF_WEEK) - 1];
+    }
+
+    public static String getFormattedDate(Resources res, Calendar date)
+    {
+        String[] monthNames = res.getStringArray(R.array.month_names);
+        return date.get(Calendar.DATE) + ". "
+                + monthNames[date.get(Calendar.MONTH)] + " "
+                + date.get(Calendar.YEAR);
+    }
+
+    public static String getShortFormattedDate(Resources res, Calendar date)
+    {
+        return getShortDayName(res, date) + ", " + date.get(Calendar.DATE) + ". " + (date.get(Calendar.MONTH) + 1) + ".";
+    }
+
+    /**
+     * Builds a localized date string with day name
+     */
+    public static String localizeDate(Resources resources, Calendar date)
+    {
+        Calendar now = Calendar.getInstance(LocaleUtil.getLocalTimezone());
+        // Check for today and tomorrow
+        if (date.get(Calendar.ERA) == now.get(Calendar.ERA) &&
+                date.get(Calendar.YEAR) == now.get(Calendar.YEAR))
+        {
+            // Today
+            if (date.get(Calendar.DAY_OF_YEAR) == now.get(Calendar.DAY_OF_YEAR))
+            {
+                return resources.getString(R.string.today);
+            }
+
+            // Add one day to now to get tomorrows date
+            now.roll(Calendar.DAY_OF_YEAR, 1);
+
+            // Tomorrow, because we added one day to now
+            if (date.get(Calendar.DAY_OF_YEAR) == now.get(Calendar.DAY_OF_YEAR))
+            {
+                return resources.getString(R.string.tomorrow);
+            }
+        }
+
+        StringBuilder dateString = new StringBuilder();
+
+        dateString.append(LocaleUtil.getDayName(resources, date) + ", ");
+        dateString.append(LocaleUtil.getFormattedDate(resources, date));
+
+        return dateString.toString();
+    }
+
+
+    public static String getStringNumberForm(Resources res, int resourceArray,
+                                             int number)
+    {
+        String[] wordArray = res.getStringArray(resourceArray);
+
+        int mod = number % 100;
+
+        switch (mod)
+        {
+            case 1:
+                return wordArray[0];
+            case 2:
+                return wordArray[1];
+            case 3:
+            case 4:
+                return wordArray[2];
+            default:
+                return wordArray[3];
+        }
+    }
+
+    public static SimpleDateFormat getSimpleDateFormat(String format)
+    {
+        // Cache SimpleDateFormat since retrieving timezone data takes alot of time
+        if (!dateFormatCache.containsKey(format))
+        {
+            SimpleDateFormat sdf = new SimpleDateFormat(format, getLocale());
+            sdf.setTimeZone(getLocalTimezone());
+            dateFormatCache.put(format, sdf);
+        }
+
+        return dateFormatCache.get(format);
+    }
+
+    public static TimeZone getLocalTimezone()
+    {
+        TimeZone tz = TimeZone.getTimeZone("Europe/Ljubljana");
+
+        if (tz.getID().equals(TimeZone.getTimeZone("GMT").getID()))
+        {
+            return TimeZone.getDefault();
+        }
+
+        return tz;
+    }
+
+    public static Locale getLocale()
+    {
+        if (localeCache == null)
+        {
+            localeCache = new Locale("sl-SI");
+        }
+        return localeCache;
+    }
+}
