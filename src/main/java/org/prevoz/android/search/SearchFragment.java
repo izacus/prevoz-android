b2ae34812597dd8e72459e6c761278bb56725d6d
Jernej Virag <jernej@virag.si>
2014-01-25 21:18:03 +0100

    Basic design for search form

--- src/main/java/org/prevoz/android/search/SearchFragment.java
+++ src/main/java/org/prevoz/android/search/SearchFragment.java
@@ -1,10 +1,39 @@
 package org.prevoz.android.search;
 
 import android.support.v4.app.Fragment;
+import com.fourmob.datetimepicker.date.DatePickerDialog;
+import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.EFragment;
 import org.prevoz.android.R;
 
+import java.util.Calendar;
+
 @EFragment(R.layout.fragment_search)
-public class SearchFragment extends Fragment
+public class SearchFragment extends Fragment implements DatePickerDialog.OnDateSetListener
 {
+
+    @Click(R.id.search_date)
+    protected void clickDate()
+    {
+        final Calendar calendar = Calendar.getInstance();
+        DatePickerDialog dialog = DatePickerDialog.newInstance(this,
+                                                               calendar.get(Calendar.YEAR),
+                                                               calendar.get(Calendar.MONTH),
+                                                               calendar.get(Calendar.DAY_OF_MONTH),
+                                                               false);
+        dialog.show(getActivity().getSupportFragmentManager(), "SearchDate");
+    }
+
+    // This is duplicated to allow clicking on logo or edittext
+    @Click(R.id.search_date_edit)
+    protected void clickDateEdit()
+    {
+        clickDate();
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog datePickerDialog, int i, int i2, int i3)
+    {
+
+    }
 }
