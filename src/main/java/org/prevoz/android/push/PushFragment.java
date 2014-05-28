31a0489d369d9d94938ecfc0753e14532afa834a
Jernej Virag <jernej@virag.si>
2014-05-28 21:31:24 +0200

    Reskinned dialog for ride cancel

--- src/main/java/org/prevoz/android/push/PushFragment.java
+++ src/main/java/org/prevoz/android/push/PushFragment.java
@@ -5,16 +5,19 @@ import android.support.v4.app.Fragment;
 import android.view.View;
 import android.widget.ListView;
 import de.greenrobot.event.EventBus;
+import eu.inmite.android.lib.dialogs.ISimpleDialogListener;
+import eu.inmite.android.lib.dialogs.SimpleDialogFragment;
 import org.androidannotations.annotations.*;
 import org.prevoz.android.R;
 import org.prevoz.android.events.Events;
 import org.prevoz.android.model.NotificationSubscription;
+import org.prevoz.android.util.LocaleUtil;
 import org.prevoz.android.util.ViewUtils;
 
 import java.util.List;
 
 @EFragment(R.layout.fragment_notifications)
-public class PushFragment extends Fragment
+public class PushFragment extends Fragment implements ISimpleDialogListener
 {
     public static final String DIALOG_ARG_SUB = "subscription";
 
@@ -27,6 +30,9 @@ public class PushFragment extends Fragment
     @Bean
     protected PushManager pushManager;
 
+    // Used for dialog callback due to unvieldy interface
+    private NotificationSubscription clickedSubscription;
+
     @AfterViews
     protected void initFragment()
     {
@@ -38,11 +44,15 @@ public class PushFragment extends Fragment
     @ItemClick(R.id.notifications_list)
     protected void itemClick(NotificationSubscription item)
     {
-        Bundle args = new Bundle();
-        args.putParcelable(DIALOG_ARG_SUB, item);
-        UnsubscribeNotificationDialog dialog = new UnsubscribeNotificationDialog_();
-        dialog.setArguments(args);
-        dialog.show(getActivity().getSupportFragmentManager(), "UnsubscribeDialog");
+        clickedSubscription = item;
+        SimpleDialogFragment.createBuilder(getActivity(), getActivity().getSupportFragmentManager())
+                            .setTitle(item.getFrom().toString() + " - " + item.getTo().toString())
+                            .setMessage(String.format("Ali se res želite odjaviti od obveščanja v %s?", LocaleUtil.getNotificationDayName(getResources(), item.getDate()).toLowerCase()))
+                            .setPositiveButtonText("Odjavi")
+                            .setNegativeButtonText("Prekliči")
+                            .setTargetFragment(this, 0)
+
+                            .show();
     }
 
     @Override
@@ -63,5 +73,20 @@ public class PushFragment extends Fragment
     {
         List<NotificationSubscription> notifications = pushManager.getSubscriptions();
         notificationList.setAdapter(new PushNotificationsAdapter(getActivity(), notifications));
+    }
+
+    @Override
+    public void onPositiveButtonClicked(int requestCode)
+    {
+        if (clickedSubscription != null)
+        {
+            pushManager.setSubscriptionStatus(clickedSubscription.getFrom(), clickedSubscription.getTo(), clickedSubscription.getDate(), false);
+        }
+    }
+
+    @Override
+    public void onNegativeButtonClicked(int requestCode)
+    {
+        clickedSubscription = null;
     }
 }
