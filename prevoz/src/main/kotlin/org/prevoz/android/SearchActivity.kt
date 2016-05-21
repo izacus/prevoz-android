1c0466695c6ae0fcdc1bb76abb3bf0734b2d4bb2
Jernej Virag <jernej@virag.si>
2016-05-21 22:47:54 +0200

    Add better search form

---
+++ prevoz/src/main/kotlin/org/prevoz/android/SearchActivity.kt
@@ -0,0 +1,13 @@
+package org.prevoz.android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_search.*
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
